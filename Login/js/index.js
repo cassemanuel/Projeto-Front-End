4b0259b728304769b5d63b9e1f7a2a6c88eab394
cassemanuel <cassioefs@dcc.ufrj.br>
2022-01-03 05:31:58 -0300

    Correções no projeto criado

--- Login/js/index.js
+++ Login/js/index.js
@@ -1,24 +1,4 @@
 function showPassword() {
-<<<<<<< HEAD
-  const eye = document.getElementById('eye');
-  const eyeSlash = document.getElementById('eye-slash');
-  const fieldPassword = document.getElementById('field-password');
-
-  if(eye.style.display === 'none') {
-    eye.style.display = 'block';
-    eyeSlash.style.display = 'none';
-    fieldPassword.type = 'text';
-  } else {
-    eye.style.display = 'none';
-    eyeSlash.style.display = 'block';
-    fieldPassword.type = 'password';
-  }
-};
-
-document.getElementById('btn-login').addEventListener('click', function(e) {
-  e.preventDefault();
-  alert('Logado!');
-=======
     const eye = document.getElementById('eye');
     const eyeSlash = document.getElementById('eye-slash');
     const fieldPassword = document.getElementById('field-password');
@@ -37,5 +17,4 @@ document.getElementById('btn-login').addEventListener('click', function(e) {
 document.getElementById('btn-login').addEventListener('click', function(e) {
     e.preventDefault();
     alert('Logado!');
->>>>>>> 439cc32 (Projeto Login Concluido)
 });
